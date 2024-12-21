65180a9a196c65a49485a83ad22eeda923e7f522
Ray Tsai <0410330@gmail.com>
2024-12-21 10:56:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ inline int hashit(std::string const &inString)
 
 int main(int argc, char *argv[])
 {
-    std::string filename = (argc > 0) ? argv[1] : "a280.tsp";
+    std::string filename = (argc > 0) ? argv[1] : "tsp_graph/a280.tsp";
     std::string type = (argc > 1) ? argv[2] : "aco";
     std::string parallel = (argc > 2) ? argv[3] : "serial";
     TSPReader reader;
@@ -42,10 +42,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    const int thread_num = 8;
     std::vector<int> bestTour;
     double bestDistance = 0.0, elapsedTime = 0.0;
     std::cout << "Solving TSP...\n";
-    int thread_num = 8;
     if (type == "aco")
     {
         auto start = std::chrono::high_resolution_clock::now();
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
         }
         else if (parallel == "thread")
         {
-            ACOThread solver(reader.getPoints(), 64);
+            ACOThread solver(reader.getPoints(), thread_num);
             solver.solve();
             bestTour = solver.getTour();
             bestDistance = solver.getDistance();
@@ -90,11 +90,7 @@ int main(int argc, char *argv[])
         }
         else if (parallel == "thread")
         {
-<<<<<<< HEAD
             ga_thread gathread(reader.getPoints(), 100, 10000, 0.05, 0.8, thread_num);
-=======
-            ga_thread gathread(reader.getPoints(), 100, 10000, 0.05, 0.8, 64);
->>>>>>> 29a43da7207be164d370feaaad26d7a343a8cc88
             start = std::chrono::high_resolution_clock::now();
             result = gathread.solve();
         }
